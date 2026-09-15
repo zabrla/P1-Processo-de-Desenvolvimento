41429f5ba5a6254b397af094d5ac656a2d9aa87c
Carlos Morais <coelhocadu0909@gmail.com>
2026-09-15 19:17:58 -0300

    feature: Cria app mensagens com rota inicial de /criptografar/

--- PdD/settings.py
+++ PdD/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mensagens',
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
