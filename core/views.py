f308dce0d126f434376ce1c0a4938f3ff11193f3
Rhennan Oliveira <reehsecundario@gmail.com>
2026-09-15 23:52:56 -0300

    feat(core): implementa autenticacao por email e view de logout

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from .forms import CadastroForm
+from .forms import LoginForm
 
 def cadastro_view(request):
     if request.user.is_authenticated:
@@ -18,4 +20,19 @@ def cadastro_view(request):
 
 
 def login_view(request):
-    return render(request, "core/login.html", {"form": None})        
+    '''if request.user.is_authenticated:
+        return redirect("mensagens:criptografar")'''
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            auth_login(request, form.cleaned_data["user"])
+            return redirect("mensagens:criptografar")
+    else:
+        form = LoginForm()
+
+    return render(request, "core/login.html", {"form": form})
+
+def logout_view(request):
+    auth_logout(request)
+    return redirect("core:login")     
