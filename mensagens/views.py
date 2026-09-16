b81c95239095794a279e82d498df983e9e8fc561
Carlos Morais <coelhocadu0909@gmail.com>
2026-09-16 11:57:24 -0300

    feature: Integra bst_core/bst_visualizacao nas views de criptografar e descriptografar

--- mensagens/views.py
+++ mensagens/views.py
@@ -1,9 +1,13 @@
-# from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.http import HttpResponse
+import os
+import uuid
 
-# from .bst_core import criptografar_mensagem      # ajustar import certo
-# from .bst_visualizacao import gerar_imagem_arvore
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from utilils.bst.bst_visualizacao import gerar_imagem_cripto, gerar_imagem_decripto
+
 
 #@login_required
 def criptografar(request):
@@ -13,16 +17,19 @@ def criptografar(request):
         email_destinatario = request.POST.get('email')
         mensagem = request.POST.get('mensagem')
 
-        # TODO: chamar a função real do bst_core.py aqui
-        # mensagem_cifrada, arvore = criptografar_mensagem(mensagem, email_destinatario)
-        # imagem_path = gerar_imagem_arvore(arvore)
+        os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+        nome_imagem = f"{uuid.uuid4().hex}.png"
+        caminho_imagem = os.path.join(settings.MEDIA_ROOT, nome_imagem)
 
-        # guarda na sessão pra "Salvar" saber o que gravar, sem recriptografar
-        #request.session['mensagem_cifrada'] = mensagem_cifrada
+        # a chave usada pra cifrar e o email do destinatario -- so quem
+        # logar com esse mesmo email consegue decifrar (ver view abaixo)
+        resultado = gerar_imagem_cripto(email_destinatario, mensagem, caminho_imagem)
+
+        request.session['mensagem_cifrada'] = resultado['cifra']
         request.session['email_destinatario'] = email_destinatario
 
-        #contexto['mensagem_cifrada'] = mensagem_cifrada
-        #contexto['imagem_arvore'] = imagem_path
+        contexto['mensagem_cifrada'] = resultado['cifra']
+        contexto['imagem_arvore'] = settings.MEDIA_URL + nome_imagem
 
     return render(request, 'criptografar.html', contexto)
 
@@ -32,11 +39,12 @@ def salvar_arquivo(request):
     mensagem_cifrada = request.session.get('mensagem_cifrada')
 
     if not mensagem_cifrada:
-        return HttpResponse(status=400) 
+        return HttpResponse(status=400)
 
     response = HttpResponse(mensagem_cifrada, content_type='text/plain')
     response['Content-Disposition'] = 'attachment; filename="mensagem_cifrada.txt"'
     return response
+
 
 #@login_required
 def descriptografar(request):
@@ -54,13 +62,18 @@ def descriptografar(request):
 
         elif acao == 'descriptografar':
             mensagem = request.POST.get('mensagem')
-
-            # TODO: chamar a função real do bst_core.py aqui
-            # mensagem_decifrada, arvore = descriptografar_mensagem(mensagem)
-            # imagem_path = gerar_imagem_arvore(arvore)
-
             contexto['mensagem'] = mensagem
-            #contexto['mensagem_decifrada'] = mensagem_decifrada
-            #contexto['imagem_arvore'] = imagem_path
+
+            os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+            nome_imagem = f"{uuid.uuid4().hex}.png"
+            caminho_imagem = os.path.join(settings.MEDIA_ROOT, nome_imagem)
+
+            try:
+                # a chave pra decifrar e o email de quem esta logado
+                resultado = gerar_imagem_decripto(request.user.email, mensagem, caminho_imagem)
+                contexto['mensagem_decifrada'] = resultado['texto']
+                contexto['imagem_arvore'] = settings.MEDIA_URL + nome_imagem
+            except Exception:
+                contexto['erro'] = 'Não foi possível descriptografar essa mensagem com o seu e-mail.'
 
     return render(request, 'descriptografar.html', contexto)
