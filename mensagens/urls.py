f2daa717ebe04f5c838f7ac3ae2bb0bc06c4c31f
Rhennan Oliveira <reehsecundario@gmail.com>
2026-09-15 23:52:37 -0300

    fix(urls): adiciona namespaces e corrige rotas de redirecionamento

--- mensagens/urls.py
+++ mensagens/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "mensagens"
+
 urlpatterns = [
     path('criptografar/', views.criptografar, name='criptografar'),
     path('criptografar/salvar/', views.salvar_arquivo, name='salvar_arquivo'),
