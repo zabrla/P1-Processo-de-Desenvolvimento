f8659007b7b9763460cdce628c71f1fbed790bfb
jjoshua08 <tratar.quitacao_0d@icloud.com>
2026-09-17 18:22:37 -0300

    Style Tela de Histórico

--- mensagens/urls.py
+++ mensagens/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = "mensagens"
 
 urlpatterns = [
+    path('historico/', TemplateView.as_view(template_name='historico.html'), name='historico'),
     path('criptografar/', views.criptografar, name='criptografar'),
     path('criptografar/salvar/', views.salvar_arquivo, name='salvar_arquivo'),
     path('descriptografar/', views.descriptografar, name='descriptografar'),
