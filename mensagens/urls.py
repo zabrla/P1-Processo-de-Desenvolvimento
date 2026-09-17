93858c87a20a743cdaf6905d9ea40c3a59a6de97
Rhennan Oliveira <reehsecundario@gmail.com>
2026-09-17 16:35:00 -0300

    feat(audit): implementa urls de visualização dos logs

--- mensagens/urls.py
+++ mensagens/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('criptografar/', views.criptografar, name='criptografar'),
     path('criptografar/salvar/', views.salvar_arquivo, name='salvar_arquivo'),
     path('descriptografar/', views.descriptografar, name='descriptografar'),
+    path('logs/', views.historico_logs, name='historico_logs'),
 ]
