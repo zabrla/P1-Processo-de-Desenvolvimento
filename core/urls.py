95e98e8eccca271d104850aecb6a463065c9fbe9
Carlos Morais <coelhocadu0909@gmail.com>
2026-09-17 22:49:48 -0300

    feat:adiciona cadastro.html

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-    path("", views.login_view, name="index"),
+    path("", views.cadastro_view, name="index"),
     path("login/", views.login_view, name="login"),
     path("cadastro/", views.cadastro_view, name="cadastro"),
     path("logout/", views.logout_view, name="logout"),
